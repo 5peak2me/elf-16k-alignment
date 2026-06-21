8d10dcf802c37449ae0b87930b4c8f5232e2b0b7
5peak2me <461260911@qq.com>
2026-06-21 16:20:10 +0800

    build: bump plugin version to 0.0.3

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.github.5peak2me.plugin.gradle"
-version = "0.0.2"
+version = "0.0.3"
 
 // Configure the build-logic plugins to target JDK 17
 // This matches the JDK used to build the project, and is not related to what is running on device.
